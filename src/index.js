177b6dbe296fabe0e9ac398b97d21e8127d9e0b4
Dave <davediemert_88@hotmail.com>
2022-05-22 20:05:54 -0700

    need to do more reading on importing... really struggling trying to pass variables back and forth

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import { general, addToDo } from "./projectsTodos";
+
+console.log(general);
+console.log(addToDo);
+
+addToDo(
+  "Mow Lawn",
+  ["Prep Lawn Mower", "Mow Lawn", "Dispose of Grass", "Clean Up"],
+  6
+);
+
+let test = general;
+console.log(test);
+
+export { general, test };
