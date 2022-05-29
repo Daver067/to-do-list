cc193c06b06f034413ab48fb510f4f91aa58526f
Dave <davediemert_88@hotmail.com>
2022-05-28 19:04:57 -0700

    completed checkmark, edit button, and delete button for toDos are all doing what they are expected. Next step is to get all projects to display in the Nav Bar

--- src/popupModules.js
+++ src/popupModules.js
@@ -46,6 +46,41 @@ function newToDoModule() {
   );
 }
 
+//edit a toDo
+function editToDoModule(toDoToEdit) {
+  const module = moduleMaker();
+  module.appendChild(
+    createNewElement(
+      "div",
+      "module-name",
+      `<div class="toDoName"><label for="name">To-Do Name:</label><input type="text" name="name" id="name" value='${toDoToEdit.name}'/></div><div class="toDoProject"><label for="projectToDoModule">Project:</label><select name="projectToDoModule" id="projectToDoModule"><option value="General">General</option></select></div>`
+    )
+  );
+  module.appendChild(
+    createNewElement(
+      "div",
+      "module-steps",
+      `<label for="steps">To-Do Steps: <span class="small">(separate steps by line break)</span></label><textarea name="steps" id="steps" cols="30" rows="10">${toDoToEdit.steps.join(
+        "\n"
+      )}</textarea>`
+    )
+  );
+  module.appendChild(
+    createNewElement(
+      "div",
+      "module-priority",
+      `<label for="priority">Priority: <span class="small">(0-9)</span></label><input type="number" name="priority" id="priority" min="0" max="9" value='${toDoToEdit.priority}'/>`
+    )
+  );
+  module.appendChild(
+    createNewElement(
+      "div",
+      "module-buttons",
+      '<button id="ok">OK</button><div></div><button id="cancel">CANCEL</button>'
+    )
+  );
+}
+
 //makes New Project popup module
 function newProjectModule() {
   const module = moduleMaker();
@@ -66,7 +101,7 @@ function newProjectModule() {
   );
 }
 
-export { newProjectModule, newToDoModule };
+export { newProjectModule, newToDoModule, editToDoModule };
 /**
  * newProjectsModule exports to eventListeners
  * newToDoModule exports to eventListeners
--- src/projectsTodos.js
+++ src/projectsTodos.js
@@ -1,26 +1,43 @@
 import { createNewElement } from "./renderPage";
 import { allImages } from "./imageLoaderAndListener";
-import { checkMarkListener } from "./eventListeners,js";
+import {
+  checkMarkListener,
+  deleteListener,
+  editListener,
+} from "./eventListeners,js";
 
 /***************************************************************
  * TODO CLASS
  **************************************************************/
 class toDo {
-  constructor(name, steps, priority) {
+  constructor(name, steps, priority, project) {
     (this.name = name),
       (this.steps = steps),
       (this.priority = priority),
-      (this.completed = false);
+      (this.completed = false),
+      (this.delete = false),
+      (this.parentProject = project),
+      (this.edit = false);
   }
 
+  //for clicking individual checkmark in toDo. makes completed status true
   clickedCheckmark = () => {
     this.completed ? (this.completed = false) : (this.completed = true);
   };
 
+  clickedDelete = () => {
+    this.delete = true;
+  };
+
+  clickedEdit = () => {
+    this.edit = true;
+  };
+
+  //creates Dom element "to-do-display" with all children according to this classes keys
   CreateDomElement = () => {
-    const toDoCard = createNewElement("div", "to-do-display", "");
+    let toDoCard = createNewElement("div", "to-do-display", "");
     if (this.completed) {
-      toDoCard.classList.add("completed");
+      toDoCard.appendChild(createNewElement("div", "completed", "COMPLETED"));
     }
     const steps = this.#stepArrayToLi();
     toDoCard.appendChild(steps);
@@ -32,14 +49,27 @@ class toDo {
       "priority-icons",
       `<div class="priority">Priority - ${this.priority}</div>`
     );
+    toDoCard = this.#priorityColorChange(toDoCard);
     const images = createNewElement("div", "images", "");
+
     const checkMark = this.#addImages(allImages.check);
     checkMark.addEventListener("click", () => {
       checkMarkListener(this);
     });
     images.appendChild(checkMark);
-    images.appendChild(this.#addImages(allImages.edit));
-    images.appendChild(this.#addImages(allImages.delete));
+
+    const editButton = this.#addImages(allImages.edit);
+    editButton.addEventListener("click", () => {
+      editListener(this);
+    });
+    images.appendChild(editButton);
+
+    const deleteBuitton = this.#addImages(allImages.delete);
+    deleteBuitton.addEventListener("click", () => {
+      deleteListener(this);
+    });
+    images.appendChild(deleteBuitton);
+
     priorityIcons.appendChild(images);
     toDoCard.appendChild(priorityIcons);
 
@@ -63,6 +93,18 @@ class toDo {
       allSteps.appendChild(thisStep);
     });
     return allSteps;
+  };
+  #priorityColorChange = (toDoCard) => {
+    if (this.priority >= 0 && this.priority <= 3) {
+      toDoCard.style.borderColor = "green";
+    }
+    if (this.priority >= 4 && this.priority <= 6) {
+      toDoCard.style.borderColor = "orange";
+    }
+    if (this.priority >= 7 && this.priority <= 9) {
+      toDoCard.style.borderColor = "red";
+    }
+    return toDoCard;
   };
 }
 
@@ -110,6 +152,29 @@ class Project {
       toDoDisplay.appendChild(dom);
     });
   };
+
+  deleteToDo = () => {
+    let index = 0;
+    this.toDos.forEach((todo) => {
+      if (todo.delete) {
+        this.toDos.splice(index, 1);
+        index--;
+      }
+      index++;
+    });
+  };
+
+  editToDo = (newToDo) => {
+    let index = 0;
+    this.toDos.forEach((todo) => {
+      if (todo.edit) {
+        let checkedOrNot = todo.completed;
+        newToDo.completed = checkedOrNot;
+        this.toDos[index] = newToDo;
+      }
+      index++;
+    });
+  };
 }
 
 /***************************************************************
@@ -118,24 +183,39 @@ class Project {
 function checkForActiveProject() {
   projects.forEach((project) => {
     if (project.active == true) {
+      project.deleteToDo();
       project.turnToDosIntoDoms();
       project.renderDoms();
     }
   });
 }
+
 /***************************************************************
  * currently this is just for building site
  ***********************************************************/
 let mowLawn = new toDo(
   "Mow Lawn",
   ["Prep Lawn Mower", "Mow Lawn", "Dispose of Grass", "Clean Up"],
-  6
+  6,
+  "General"
+);
+let mowLawn2 = new toDo(
+  "Mow Lawn",
+  ["Prep Lawn Mower", "Mow Lawn", "Dispose of Grass", "Clean Up"],
+  8,
+  "General"
+);
+let mowLawn3 = new toDo(
+  "Mow Lawn",
+  ["Prep Lawn Mower", "Mow Lawn", "Dispose of Grass", "Clean Up"],
+  1,
+  "General"
 );
 
 let General = new Project("General");
 General.addToDo(mowLawn);
-General.addToDo(mowLawn);
-General.addToDo(mowLawn);
+General.addToDo(mowLawn2);
+General.addToDo(mowLawn3);
 
 let projects = ["junk", "more junk", General, "test"];
 
